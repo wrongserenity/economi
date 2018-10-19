265c9601becd10e080bfa41a9f0f5760091cc863
wrongserenity <wrongserenity@gmail.com>
2018-10-19 21:06:28 +0300

    upd
    другая механика

--- classes.py
+++ classes.py
@@ -3,46 +3,33 @@ import sqlite3
 
 class Player:
     """Class of players"""
-    def __init__(self, id, name, m_r_cur, st_fund):
+    def __init__(self, p_id, nm, st_fund):
         """Initiate starting parameters: id, name, """
-        self.id = id
-        self.name = name                        # player's name
-        self.ratio = float(count_ratio(id))      # ratio to main currency
-        self.main_fund_old
-        self.fund = dict([(id, st_fund), (abs(id - 1), 0), (abs(id - 2), 0), (abs(id - 3), 0)])     # dictionary of
-        self.info_country = dict([(n, self.name), (s_m_r_c, self.main_ratio_currency), (f, self.fund)])   #
-        # creating parameter dictionary of country
+        self.player_id = p_id
+        self.name = nm                        # player's name
+        self.fund = st_fund
+        self.units = {}
+        self.info_country = {'id': self.player_id, 'name': self.name, 'units': self.units, 'fund': self.fund}
 
     def check_status(self):
         """Printing all information about country"""
-        self.fund = self.count_fund()
-         #
         print(self.info_country)
 
-    def count_fund_new(self):
-        """Count fund of county"""
-        return sum(self.fund[i] * count_ratio_old[i] for i in range(1,5))
+    def start_making_unit(self):
+        pass
+
+    def creating_unit
 
 
-    def purchase_currency_on_own(self, num_player, value, ratio):
-        """Buying currency from another county"""
+class Unit(self):
+    """Created units by players"""
+    def __init__(self, player_id):
+        self.
 
-        self.fund += (self.fund.[ - value) * player_ratio[num_player]
 
-class Unit:
+class Market:
     pass
 
-
-
-def count_ratio_new(id):
-
-
-def count_ratio_for_n_player(id):
-    half_sum = sum(main_fund_old[x] for x in range(1, 5)) / 2
-
-
-
-main_fund_old = {1: 1.fund[1]}
 
 
 
