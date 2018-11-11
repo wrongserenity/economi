e5d868e31d3266487de9c8b479a5cca08569e8f1
Nick Yefremov <e_n_k@list.ru>
2018-11-11 08:03:45 +0300

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -1,5 +1,24 @@
-# import pymongo
+from pymongo import MongoClient
 
-
-def get_units(id_):
-    pass
+# TODO: write try:.. except.. wrappers
+сlass MongoConnection:
+    def __init__(self, db_name, col_name, **mongo_configs):
+        self.client = MongoClient(**mongo_configs)
+        self.db = self.client[db_name]
+        self.collection = self.db[col_name]
+    
+    def get_unit(self, unit_id):
+        return self.collection.find_one({"unit_id": unit_id})
+    
+    def set_unit(self, unit_obj):
+        self.collection.insert_one(unit_obj.to_dict())
+    
+    def count_units(self, owner_id):
+        return self.collection.count_documents({"owner_id": owner_id})
+    
+    def update_unit(self, unit_id, updates):
+        self.collection.update_one({"unit_id": unit_id}, {$set: updates})
+        
+    def close_connection(self):
+        self.client.close()
+        
