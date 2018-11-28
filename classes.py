c52ac55e913aaba3b9b5b30183a8b4dc3da043f5
wrongserenity <wrongserenity@gmail.com>
2018-11-29 02:12:22 +0300

    upd

--- classes.py
+++ classes.py
@@ -30,7 +30,8 @@ class Player:
             self.units = [Unit(data=data) for data in get_units(self.id_)]
 
     def save(self):
-        set_data(self)
+        # хз шо с ентим делать, если честно
+        PostgresConnection.set_data(user_obj)
 
     def income_fund(self, profit):
         self.fund += profit
@@ -57,11 +58,6 @@ class Player:
         return list(self.__dict__.values())
 
 
-class UnitMaker:
-    def __init__(self):
-        id_ = uuid.uuid4()
-
-
 class Unit:
     def __init__(self, id_=None, cast_time=None, cast_cost=None, st_prod=None, data=None):
         if data:
@@ -85,25 +81,51 @@ class Unit:
         return self.__dict__
             
 
+# TODO: Senpai Nikita, notice me
 class Game:
     def __init__(self):
-        pass
+        self.old_rate
 
     def next_move(self, players):
         for p in players:
             p.income_fund(...)
 
+    def rate_calc_first(self, players):
+        # тут тип надо сложить сумму всех фондов в собственной валюте игроков
+        # а потом разделить значение одного на общее и распихать это все либо
+        # в словарь, либо в список
+        for p in players:
+            pass
 
-class Market:
+    def rate_calc(self, players, id_):
+        # а эта штука считает уже в последующие разы, когда надо учитывать сколько
+        # у кого чужой валюты, причем чтобы было удобнее считать без рекурсивной хуйни,
+        # можно использовать два поля old_rate и на его основании проводить подсчет
+        for p in players:
+            pass
+
+
+# разберись с ентим дерьмом
+class UnitSell:
     def __init__(self):
         self.units_to_sell = {}
-        self.units = {}         # разберись
-    
+        self.units = {}
+
     def buy(self):
         pass
 
-    def sell(self):
+
+class Market(UnitSell):
+    def __init__(self):
         pass
 
     def process(self):
+        pass
+
+
+class Exchange(UnitSell):
+    def __init__(self):
+        pass
+
+    def sell(self):
         pass
