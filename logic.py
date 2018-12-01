827c687255fc9e0ad1b24aef16ba7a23dbf18b99
wrongserenity <wrongserenity@gmail.com>
2018-12-01 14:51:48 +0300

    upd

--- logic.py
+++ logic.py
@@ -7,7 +7,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class MainWin(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
-        self.ui = MainWindow()
+        self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
         self.menu_opened = False
