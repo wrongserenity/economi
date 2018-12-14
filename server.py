b85ace6c9fdf4d4776e0f933b7c429d046a88939
wrongserenity <wrongserenity@gmail.com>
2018-12-15 00:24:24 +0300

    oooops

--- server.py
+++ server.py
@@ -119,7 +119,7 @@ class EconomiTcpServer(object):
                 elif data["action"] == "get_uid":
                     out = self.postgres_connection.get_uid()
                 elif data["action"] == "get_player_data":
-                    out = self.get_player_data(data["args"]["id"])
+                    out = self.get_player_data(data["args"]["uid"])
                 else:
                     out = "Error occurred"
 
