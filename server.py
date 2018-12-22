5259e7ccc4723acc805ffc3f921d372bb73a6606
wrongserenity <wrongserenity@gmail.com>
2018-12-22 04:03:32 +0300

    Update server.py

--- server.py
+++ server.py
@@ -81,6 +81,8 @@ class EconomiTcpServer(object):
     @staticmethod
     def format_(obj, out=False):
         print(str(obj))
+        if "GET" in obj.decode("utf-8"):
+            return {}
         return bytes(f'{json.dumps(obj)}\n', "utf-8") if out else json.loads(obj.decode("utf-8"))
 
     @tornado.gen.coroutine
